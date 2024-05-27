6d3d59e5dc35a889bcf3aa0ba5584179f4776f3d
big-bones <sarangsd01@gmail.com>
2024-05-27 10:16:36 +0530

    Add files via upload

---
+++ Sample_Codes.cs
@@ -0,0 +1,46 @@
+#Sample program for events using delegates internally
+
+using System;
+
+public delegate void MyDelegate(int a,int b);
+
+class Publisher{
+    
+    private MyDelegate myDelegate;
+    
+    public event MyDelegate MyEvent{
+        add{
+            myDelegate += value;
+        }
+        remove{
+            myDelegate -= value;
+        }
+    }
+    
+    public void RaiseEvent(int a,int b){
+        myDelegate(a,b);
+    }
+    
+}
+
+class Subscriber{
+    public void Add(int a,int b){
+        Console.WriteLine(a + b);
+    }
+    public void Multiply(int a,int b){
+        Console.WriteLine(a*b);
+    }
+}
+
+class Practice{
+  static void Main() {
+    Subscriber sc = new Subscriber();
+    Publisher pbs = new Publisher();
+    pbs.MyEvent += sc.Add;
+    pbs.MyEvent += sc.Multiply;
+    pbs.RaiseEvent(10,20);
+  }
+}
+
+--------------------------------------------------------------------------------------------------
+
