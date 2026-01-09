b0f69bebe0a498e2ef12d89f7bd0db34cbe971a4
big-bones <sarangsd01@gmail.com>
2026-01-09 09:36:28 +0530

    Modify SqlConnection initialization
    Updated the SqlConnection string to an empty value.

--- Sprint-2/Stored-Procedure-Transaction.cs
+++ Sprint-2/Stored-Procedure-Transaction.cs
@@ -39,7 +39,7 @@ class Program
 {
     static void Main()
     {
-		using (SqlConnection conn = new SqlConnection("Data Source=EPINHYDW0FCC\\SQLEXPRESS;Initial Catalog=learming;Integrated Security=SSPI"))
+		using (SqlConnection conn = new SqlConnection(""))
 		{
             conn.Open();
             using (SqlCommand cmd = new SqlCommand("UPDATE_EMPLOYEE_SALARY",conn))
